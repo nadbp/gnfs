addc4c0da5566aff5b363f3cebc4015776167115
nadbp <nadbpwater@gmail.com>
2017-02-07 12:57:27 -0500

    remove copyright

--- src/greeter_client.cc
+++ src/greeter_client.cc
@@ -1,24 +1,3 @@
-/*
-  FUSE: Filesystem in Userspace
-  Copyright (C) 2001-2007  Miklos Szeredi <miklos@szeredi.hu>
-
-  This program can be distributed under the terms of the GNU GPL.
-  See the file COPYING.
-*/
-
-/** @file
- *
- * minimal example filesystem using high-level API
- *
- * Compile with:
- *
- *     gcc -Wall hello.c `pkg-config fuse3 --cflags --libs` -o hello
- *
- * ## Source code ##
- * \include hello.c
- */
-
-
 #define FUSE_USE_VERSION 30
 
 //#include <config.h>
