1473eddb19b9f775e72024809d8a2be21084e2aa
ArtoriaRen <s200652168@hotmail.com>
2017-02-20 09:45:31 -0500

    write a c program to test open(), read(), write()

---
+++ src/ReadWrite.cpp
@@ -0,0 +1,51 @@
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <errno.h>
+#include <iostream>
+#include <unistd.h>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+if (argc<2){
+  cout<<"Please input file name."<<endl;
+  cout<<"argc="<<argc<<endl;
+  return 1;
+}
+
+int fd;
+u_int size=32;
+int offset=0;
+int read_bytes, write_bytes;
+char * buffer = (char*)malloc(size); 
+fd = open (argv[1] ,O_RDWR);
+if (fd<0)
+  perror(strerror(errno));
+std::cout<<"open file handle="<<fd<<std::endl;
+
+read_bytes = pread(fd,buffer,size, offset);
+if (read_bytes<0)
+  perror(strerror(errno));
+cout<<"content read="<<*buffer<<endl;
+cout<<"# of bytes read="<<read_bytes<<endl;
+
+write_bytes = pwrite(fd,buffer,read_bytes,offset+size);
+if (write_bytes<0)
+  perror(strerror(errno));
+cout<<"# of bytes written="<<write_bytes<<endl;
+
+if (fd >0){
+  cout<<"closing the fd="<<fd<<endl;
+  close(fd);
+  cout<<"closing finish. fd="<<fd<<endl;
+}
+      
+free(buffer);
+
+return 0;
+}
